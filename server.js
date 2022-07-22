6577ea0a3e068ace08b9c4e515d5d2cd6d124760
Stefan Nikolovski <ni.stefan.95@gmail.com>
2022-07-22 19:30:01 +0200

    limit change

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ MongoClient.connect(process.env.MONGO_HOST)
 function bootstrap() {
     const app = express();
     app.use(cors());
-    app.use(express.json());
+    app.use(express.json({limit: '50mb'}));
     app.get('/', (req, res) => {
         res.end('Hello from Express!');
     });
